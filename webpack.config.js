9fce9b4290409faecb1db5f5fcafe0dbf9e1dc91
H0onnn <dukei2017@naver.com>
2023-09-12 18:09:54 +0900

    Init : project setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -32,9 +34,13 @@ module.exports = {
     ],
   },
   plugins: [
+    new Dotenv(),
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: 'index.html',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'public/images', to: 'images' }],
     }),
   ],
   devServer: {

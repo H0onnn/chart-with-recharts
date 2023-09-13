39140872c0d2635c6b35e2cff42b6f2cf4219780
H0onnn <dukei2017@naver.com>
2023-09-13 17:01:27 +0900

    Feat : 전체 페이지 레아웃 작성

--- src/layout/PageLayout.tsx
+++ src/layout/PageLayout.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import styled from 'styled-components';
+import Header from '../components/UI/Header';
 
-const PageLayout = () => {
-  return <div>PageLayout</div>;
+const PageLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Header />
+      <MainLayout>
+        <main>{children}</main>
+      </MainLayout>
+    </>
+  );
 };
 
 export default PageLayout;
+
+const MainLayout = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 170px auto 0 auto;
+  padding: 40px 20px 20px 20px;
+  max-width: 1200px;
+  width: 100%;
+  height: calc(100vh - 170px - 80px);
+  box-sizing: border-box;
+`;
